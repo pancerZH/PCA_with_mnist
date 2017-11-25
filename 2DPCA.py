5f6f29980346f00c8bcd20cfafcb5c47a0404edd
DESKTOP-DLOBL4I\张文喆 <942740938@qq.com>
2017-11-25 20:54:31 +0800

    对图片使用2DPCA降维至10x10

--- 2DPCA.py
+++ 2DPCA.py
@@ -3,9 +3,8 @@ from PIL import Image
 '''
 imgs 是三维的图像矩阵，第一维是图像的个数
 '''
-def TwoDPCA(imgs,p):
+def TwoDPCA(imgs, dim):
     a,b,c = imgs.shape
-    print(a,b,c)
     average = np.zeros((b,c))
     for i in range(a):
         average += imgs[i,:,:]/(a*1.0)
@@ -15,26 +14,32 @@ def TwoDPCA(imgs,p):
         temp = img-average
         G_t = G_t + np.dot(temp.T,temp)/(a*1.0)
     w,v = np.linalg.eigh(G_t)
+    # print('v_shape:{}'.format(v.shape))
     w = w[::-1]
     v = v[::-1]
-    print(w)
+    '''
     for k in range(c):
-        alpha = sum(w[:k])*1.0/sum(w)
+        # alpha = sum(w[:k])*1.0/sum(w)
+        alpha = 0
         if alpha >= p:
             u = v[:,:k]
             break
+    '''
+    u = v[:,:dim]
     return u
 
 
-def TTwoDPCA(imgs,p):
-    u = TwoDPCA(imgs,p)
+def TTwoDPCA(imgs, dim):
+    u = TwoDPCA(imgs, dim)
     a1,b1,c1 = imgs.shape
     img = []
     for i in range(a1):
         temp1 = np.dot(imgs[i,:,:],u)
-        img.append(temp1.T)
+        img.append(temp1)
     img = np.array(img)
-    uu = TwoDPCA(img,p)
+    print('img_shape:{}'.format(img.shape))
+    uu = TwoDPCA(img, dim)
+    print('uu_shape:{}'.format(uu.shape))
     return u,uu
 
 if __name__ == '__main__':
@@ -45,5 +50,8 @@ if __name__ == '__main__':
     data = im_grey.getdata()
     data = np.array(data)
     data2 = data.reshape(1, a, b)
-    data2_2DPCA = TwoDPCA(data2, 0.9)
-    print(data2_2DPCA.shape)
+    print('data2_shape:{}'.format(data2.shape))
+    data2_2DPCA, data2_2D2DPCA = TTwoDPCA(data2, 10)
+    print('data2_2DPCA:{}'.format(data2_2DPCA.shape))
+    print('data2_2D2DPCA:{}'.format(data2_2D2DPCA.shape))
+    
